cb53775165be77ee73e16101b9df0892d2dae169
Py Syun <pysyun@vitche.com>
2019-03-02 23:57:24 +0200

    - module installer

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+   name = 'pysyun_timeline',
+   version = '1.0',
+   description = 'Syun\'s Python SDK for time series analysis. Python toolkit for analyzing time series data.',
+   author = 'Py Syun',
+   author_email = 'pysyun@vitche.com',
+   py_modules = ['algebra', 'converters', 'filters', 'reducers', 'segmenters', 'sources'],
+   install_requires = ['re']
+)
