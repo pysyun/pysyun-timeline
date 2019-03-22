d20dc22b016fbf7d73d16355f6acfb6fa1e66bbc
pysyun <48061047+pysyun@users.noreply.github.com>
2019-03-22 19:19:00 +0200

    - event count aggregate

--- pysyun/timeline/statistics.py
+++ pysyun/timeline/statistics.py
@@ -16,3 +16,49 @@ class MedianAggregateIndexes:
             indexes.append(i + section.index(median))
             i = i + step
         return indexes
+
+class EventCountAggregate:
+
+    def __init__(self, intervalCount):
+        self.intervalCount = intervalCount
+
+    def process(self, timeLine):
+        newTimeLine = []
+        
+        timeLine.sort(key=lambda value: value['time'])
+
+        # Minimum and maximum time stamps
+        i = 0
+        minimum = timeLine[0]['time']
+        maximum = minimum
+        while i < len(timeLine):
+            currentEventTime = timeLine[i]['time']
+            if currentEventTime < minimum:
+                minimum = currentEventTime
+            if maximum < currentEventTime:
+                maximum = currentEventTime
+            i = i + 1
+        
+        # Desired interval length
+        step = int((maximum - minimum) / self.intervalCount)
+        
+        # Process each interval
+        i = 0
+        length = len(timeLine)
+        currentIntervalStart = minimum
+        currentIntervalEnd = currentIntervalStart + step
+        currentEventCount = 0
+        while i < length:
+            currentEventTime = timeLine[i]['time']
+            currentEventCount = currentEventCount + 1
+            if currentIntervalEnd <= currentEventTime or i == length - 1:
+                newTimeLine.append({
+                    'time': currentEventTime,
+                    'value': currentEventCount
+                })
+                currentEventCount = 0
+                currentIntervalStart = currentIntervalStart + step
+                currentIntervalEnd = currentIntervalEnd + step
+            i = i + 1
+
+        return newTimeLine
