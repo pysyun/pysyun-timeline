eebb1aed9be128692c665faba5d71fc4b538f9e3
pysyun <48061047+pysyun@users.noreply.github.com>
2019-08-23 12:47:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup (
    description = 'Syun\'s Python SDK for time series analysis. Python toolkit for analyzing time series data.',
    author = 'Py Syun',
    author_email = 'pysyun@vitche.com',
-   py_modules = ['pysyun.timeline.algebra', 'pysyun.timeline.converters', 'pysyun.timeline.filters', 'pysyun.timeline.reducers', 'pysyun.timeline.segmenters', 'pysyun.timeline.sources', 'pysyun.timeline.statistics', 'pysyun.timeline.renderers'],
+   py_modules = ['pysyun.timeline.algebra', 'pysyun.timeline.converters', 'pysyun.timeline.filters', 'pysyun.timeline.reducers', 'pysyun.timeline.segmenters', 'pysyun.timeline.sources', 'pysyun.timeline.statistics', 'pysyun.timeline.renderers', 'pysyun.timeline.pipeline'],
    install_requires = []
 )
