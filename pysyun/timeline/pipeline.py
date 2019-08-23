2dfa95fcc05f3039fdcbf0167425b78b0b58080d
pysyun <48061047+pysyun@users.noreply.github.com>
2019-08-23 12:46:46 +0300

    Create pipeline.py

---
+++ pysyun/timeline/pipeline.py
@@ -0,0 +1,44 @@
+import time
+
+from compute.graph.structure import Node
+from compute.graph.structure import Synapse
+
+class PipelineNode(Node):
+
+    def __init__(self, processor):
+        self.__processor = processor
+        super().__init__()
+
+    def __str__(self):
+        return self.__processor.__str__()
+
+    def add(self, neighbor):
+        if isinstance(neighbor, Synapse):
+            super().add(neighbor)
+        else:
+            super().add(Synapse().add(neighbor))
+        return self
+
+    def activate(self):
+
+        super().activate()
+
+        # Write profiling data
+        synapses = self.synapses()
+        for i in range(len(synapses)):
+            neighborNode = synapses[i].node()
+            if isinstance(neighborNode, ProfileNode):
+                neighborNode.write([self.delta])        
+        
+    def process(self):
+        data = self.read()
+        startAmount = len(data) if None != data else 0
+        startTime = time.time()
+        data = self.__processor.process(data)
+        endTime = time.time()
+        endAmount = len(data) if None != data else 0
+        self.delta = {
+            "amount": endAmount - startAmount,
+            "time": endTime - startTime
+        }
+        self.write(data)
