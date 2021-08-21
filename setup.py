15a40bb1cd8a97c966608c2ee575c95555e788ba
pysyun <48061047+pysyun@users.noreply.github.com>
2021-08-21 17:50:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
    name = 'pysyun_timeline',
-   version = '1.0',
+   version = '1.1',
    description = 'Syun\'s Python SDK for time series analysis. Python toolkit for analyzing time series data.',
    author = 'Py Syun',
    author_email = 'pysyun@vitche.com',
