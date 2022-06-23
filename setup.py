ab022f72c3fa81db60e4f4a9dc236108352dc8b5
Py Syun <pysyun@vitche.com>
2022-06-24 01:06:11 +0300

    - added referenced third-party libraries.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
-setup (
-   name = 'pysyun_timeline',
-   version = '1.1',
-   description = 'Syun\'s Python SDK for time series analysis. Python toolkit for analyzing time series data.',
-   author = 'Py Syun',
-   author_email = 'pysyun@vitche.com',
-   py_modules = ['pysyun.timeline.algebra', 'pysyun.timeline.converters', 'pysyun.timeline.filters', 'pysyun.timeline.graph', 'pysyun.timeline.reducers', 'pysyun.timeline.segmenters', 'pysyun.timeline.sources', 'pysyun.timeline.statistics', 'pysyun.timeline.renderers', 'pysyun.timeline.pipeline'],
-   install_requires = []
+setup(
+    name='pysyun_timeline',
+    version='1.2',
+    description='Syun\'s Python SDK for time series analysis. Python toolkit for analyzing time series data.',
+    author='Py Syun',
+    author_email='pysyun@vitche.com',
+    py_modules=['pysyun.timeline.algebra', 'pysyun.timeline.converters', 'pysyun.timeline.filters',
+                'pysyun.timeline.graph', 'pysyun.timeline.reducers', 'pysyun.timeline.segmenters',
+                'pysyun.timeline.sources', 'pysyun.timeline.statistics', 'pysyun.timeline.renderers',
+                'pysyun.timeline.pipeline'],
+    install_requires=['requests', 'pymongo', 'pandas', 'scipy', 'sklearn', 'beautifulsoup4',
+                      'git+https://github.com/vitche/python-storage-timeline-client.git']
 )
