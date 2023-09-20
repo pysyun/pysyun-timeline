3b32816164656517ea327ebcb178615baebacb58
Ira <immortaltapok@lineardev.net>
2023-09-20 21:07:32 +0300

    add ValueChanges

--- pysyun/timeline/filters.py
+++ pysyun/timeline/filters.py
@@ -18,18 +18,18 @@ from sklearn import preprocessing
 
 # Removes all exact matches from a time-line according to the black list
 class BlackList:
-    
+
     def __init__(self, expressions):
         self.expressions = expressions
-    
+
     def process(self, timeLine):
         results = []
         for j in range(len(timeLine)):
             segment = timeLine[j]['value']
             for k in range(len(self.expressions)):
-                segment.discard(self.expressions[k])   
+                segment.discard(self.expressions[k])
             if len(segment) != 0:
-                results.append ({
+                results.append({
                     'time': timeLine[j]['time'],
                     'value': segment
                 })
@@ -47,27 +47,28 @@ class WhiteList:
             segment = timeLine[i]['value']
             if isinstance(segment, int) or isinstance(segment, str):
                 if segment in self.values:
-                    results.append ({
+                    results.append({
                         'time': timeLine[i]['time'],
                         'value': segment
-                    })                
+                    })
             else:
                 segment = filter(lambda x: x in self.values, segment)
                 segment = list(segment)
                 if len(segment) != 0:
-                    results.append ({
+                    results.append({
                         'time': timeLine[i]['time'],
                         'value': segment
                     })
-        return results     
+        return results
+
+    # Takes all regular expression matches from a time-line according to the regular expressions white list
 
 
-# Takes all regular expression matches from a time-line according to the regular expressions white list
 class RegularExpressionWhiteList:
-    
+
     def __init__(self, expressions):
         self.expressions = expressions
-    
+
     def process(self, timeLine):
         results = []
         for i in range(len(timeLine)):
@@ -77,7 +78,7 @@ class RegularExpressionWhiteList:
                     if None != re.match(expression, segment):
                         segments.append(segment)
             if len(segments):
-                results.append ({
+                results.append({
                     'time': timeLine[i]['time'],
                     'value': segments
                 })
@@ -122,7 +123,7 @@ class TelegramInvitationPureHyperlinks(TelegramInvitationHyperlinks):
                 segment = parsed.scheme + '://' + parsed.netloc + '/' + search.group(1)
                 segments.append(segment)
             if len(segments):
-                results.append ({
+                results.append({
                     'time': timeLine[i]['time'],
                     'value': segments
                 })
@@ -195,7 +196,7 @@ class ValueChangeIntervals:
         results = []
 
         for index in range(len(timeLine)):
-            
+
             time = timeLine[index]['time']
             currentValue = timeLine[index]['value']
             previousValue = currentValue
@@ -212,27 +213,43 @@ class ValueChangeIntervals:
         return results
 
 
+class ValueChanges:
+    def process(self, timeLine):
+        results = []
+
+        for index in range(len(timeLine)):
+            if index == 0:
+                results.append(timeLine[index])
+            else:
+                currentValue = timeLine[index]['value']
+                previousValue = timeLine[index - 1]['value']
+                if currentValue != previousValue:
+                    results.append(timeLine[index])
+
+        return results
+
+
 class KMeansClustering:
 
-    def __init__(self, clusterCount = None):
-        self.clusterCount = clusterCount    
-    
+    def __init__(self, clusterCount=None):
+        self.clusterCount = clusterCount
+
     def process(self, timeLine):
-        
+
         # Convert to the data frame
-        data = {'time':[]}
+        data = {'time': []}
         for i in range(len(timeLine)):
             data['time'].append(timeLine[i]['time'])
         data = DataFrame(data)
         dataForClust = data.values
-        
+
         # Pre-processing (scaling)
         scaler = preprocessing.StandardScaler()
         dataNorm = scaler.fit_transform(dataForClust)
-        
+
         # Convert to the data frame
         dataNorm = DataFrame(dataNorm, columns=['time'])
-        
+
         # Eucledian distance
         dataDist = pdist(dataNorm, 'euclidean')
 
@@ -241,33 +258,33 @@ class KMeansClustering:
 
         # The "elbow" method to evaluate optimal segment count
         if self.clusterCount is None:
-        
+
             start = 1
             before = 20
-          
+
             samples = []
             for k in range(start, before):
                 kmSamples = KMeans(n_clusters=k, n_init=before, random_state=1)
                 kmSamples.fit(dataLinkage)
                 samples.append(kmSamples.inertia_)
-        
+
             x1, y1 = start, samples[0]
-            x2, y2 = (before - 1), samples[len(samples)-1]
-        
+            x2, y2 = (before - 1), samples[len(samples) - 1]
+
             # Find the optimum cluster count
             distances = []
             for i in range(len(samples)):
                 x0 = i + start
                 y0 = samples[i]
-                numerator = abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1)
-                denominator = math.sqrt((y2 - y1)**2 + (x2 - x1)**2)
+                numerator = abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1)
+                denominator = math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
                 distances.append(numerator / denominator)
-    
+
             self.clusterCount = distances.index(max(distances)) + start
-        
+
         # Perform K-means clustering
         km = KMeans(n_clusters=self.clusterCount, n_init=10, random_state=1).fit(dataNorm)
-        km = list(km.labels_ +1)
+        km = list(km.labels_ + 1)
 
         # Re-index cluster identifiers to make them sequential
         reindexedClusters = []
@@ -286,10 +303,10 @@ class KMeansClustering:
 
         results = []
         for i in range(len(data)):
-          results.append({
-              'time': data['time'][i],
-              'value': reindexedClusters[i]
-          })
+            results.append({
+                'time': data['time'][i],
+                'value': reindexedClusters[i]
+            })
 
         return results
 
@@ -299,11 +316,11 @@ class ClusterCentroid:
     def process(self, timeLine, timeLineClusters):
 
         # Extracting time-line values
-        data = {'value':[]}
+        data = {'value': []}
         for i in range(len(timeLine)):
-            data['value'].append(timeLine[i]['value']) 
+            data['value'].append(timeLine[i]['value'])
 
-        # Look for cluster groups count from timeLineClusters
+            # Look for cluster groups count from timeLineClusters
         # Find interval start and end indexes for each group
         clusters = []
         timeLineLength = len(timeLineClusters)
@@ -317,7 +334,7 @@ class ClusterCentroid:
                         clusters.append(interval)
                     else:
                         interval = [(clusters[-1][-1]) + 1, index]
-                        clusters.append(interval)  
+                        clusters.append(interval)
                 elif index == (timeLineLength - 1):
                     interval = [(clusters[-1][-1]) + 1, timeLineLength - 1]
                     clusters.append(interval)
@@ -339,7 +356,7 @@ class ClusterCentroid:
 
 
 class Lowercase:
-    
+
     def process(self, timeLine):
         for j in range(len(timeLine)):
             segment = timeLine[j]['value']
@@ -349,11 +366,11 @@ class Lowercase:
                 for k in range(len(segment)):
                     segment[k] = segment[k].lower()
             timeLine[j]['value'] = segment
-        return timeLine 
+        return timeLine
 
 
 class CharacterBlackList:
-    
+
     def __init__(self, substrings):
         self.substrings = substrings
 
@@ -369,14 +386,14 @@ class CharacterBlackList:
                     for k in range(len(self.substrings)):
                         segment[i] = segment[i].strip(self.substrings[k])
             timeLine[j]['value'] = segment
-        return timeLine        
+        return timeLine
 
 
 class LambdaProjection:
-    
+
     def __init__(self, projection):
         self.projection = projection
-        
+
     def process(self, timeLine):
         result = []
         for i in range(len(timeLine)):
@@ -384,19 +401,19 @@ class LambdaProjection:
             projection = self.projection(event)
             if None != projection:
                 result.append(projection)
-            
+
         return result
 
 
 class JSON:
-    
+
     def process(self, timeLine):
         result = []
         for i in range(len(timeLine)):
             event = timeLine[i]
             event["value"] = json.loads(event["value"])
             result.append(event)
-            
+
         return result
 
 
@@ -404,6 +421,7 @@ class Limit:
     def __init__(self, start, end):
         self.start = start
         self.end = end
+
     def process(self, timeLine):
         return timeLine[self.start:self.end]
 
@@ -426,6 +444,7 @@ class LastTimeFrame:
             if self.start <= value['time']:
                 newTimeLine.append(value)
         return newTimeLine
+
     def startTime(self, delta):
         currentDate = datetime.today()
         currentDate = datetime(currentDate.year, currentDate.month, currentDate.day)
@@ -435,27 +454,27 @@ class LastTimeFrame:
 
 class LastMinutes(LastTimeFrame):
     def __init__(self, count):
-        self.startTime(timedelta(minutes = count))
+        self.startTime(timedelta(minutes=count))
 
 
 class LastHours(LastTimeFrame):
     def __init__(self, count):
-        self.startTime(timedelta(hours = count))
+        self.startTime(timedelta(hours=count))
 
 
 class LastDays(LastTimeFrame):
     def __init__(self, count):
-        self.startTime(timedelta(days = count))
+        self.startTime(timedelta(days=count))
 
 
 class LastWeeks(LastTimeFrame):
     def __init__(self, count):
-        self.startTime(timedelta(weeks = count))
+        self.startTime(timedelta(weeks=count))
 
 
 class LastMonths(LastTimeFrame):
     def __init__(self, count):
-        self.startTime(timedelta(months = count))
+        self.startTime(timedelta(months=count))
 
 
 class CopyAIAugmentation:
@@ -525,11 +544,11 @@ class CopyAIAugmentation:
 
 class GPT2Transformer:
 
-  def __init__(self, length, sentences):
-    self.length = length
-    self.sentences = sentences
+    def __init__(self, length, sentences):
+        self.length = length
+        self.sentences = sentences
 
-  def process(self, data):
+    def process(self, data):
 
         from transformers import pipeline
         generator = pipeline('text-generation', model='gpt2')
@@ -538,22 +557,22 @@ class GPT2Transformer:
         for item in data:
 
             sentences = []
-            
+
             response = generator(
-              item, 
-              do_sample=True, 
-              top_k=50, 
-              temperature=0.6, 
-              max_length=self.length, 
-              num_return_sequences=self.sentences
+                item,
+                do_sample=True,
+                top_k=50,
+                temperature=0.6,
+                max_length=self.length,
+                num_return_sequences=self.sentences
             )
 
             for sentence in response:
-              sentence = sentence["generated_text"]
-              last_fullstop = sentence.rfind(".")
-              sentence = sentence[:last_fullstop+1]
-              sentences.append(sentence)
-              
+                sentence = sentence["generated_text"]
+                last_fullstop = sentence.rfind(".")
+                sentence = sentence[:last_fullstop + 1]
+                sentences.append(sentence)
+
             results.append(sentences)
 
         return results
